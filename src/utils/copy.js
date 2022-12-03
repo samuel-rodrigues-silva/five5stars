bd2c44d53a3a25d81da7c4457d9653e68f76c184
Samuel Rodrigues da Silva <samuka_55@hotmail.com>
2022-12-03 13:36:48 -0300

    feat: ui tweaks

---
+++ src/utils/copy.js
@@ -0,0 +1,59 @@
+export const STARS = [
+  { desc: '️VALORIZAÇÃO E SATISFAÇÃO' },
+  { desc: '️️PROMOÇÃO DO DESESTRESS E BEM ESTAR' },
+  {
+    desc: '️SEU DESENVOLVIMENTO AMPARADO PELA CIÊNCIA E COMPROVADO',
+  },
+  { desc: '️️PROFISSIONAL GABARITADO' },
+  {
+    desc: '️DESENVOLVIMENTO DE HABILIDADES E NEUTRALIZAÇÃO DE DEFEITO',
+  },
+];
+
+export const ATTACH = [
+  {
+    desc: 'O melhor projeto para você descobrir quem é, mais do que imagina é,  se chama FIVE STARS JJ. Quero te surpreender com a sofisticada entrega que o método SPRINT DE RESULTADOS, pode gerar na sua vida.',
+  },
+  {
+    desc: 'Dia 10/12, sábado - das 10:10 as 11:50 - na academia strongfight (Vicent Simões, 1250) ',
+  },
+  {
+    desc: '️️Você terá a oportunidade através desse convite, de saber exatamente como e quais são as promessas desse projeto a você. ',
+  },
+  { desc: 'Para se inscrever é gratuito, através do link na bio.' },
+  {
+    desc: '️Após inscrição aguarde para saber se houve confirmação de sua vaga. Serão apenas 20 vagas para fazer parte desse time 5 estrelas.',
+  },
+];
+
+export const MEDALS = [
+  '2x PAN',
+  '3x SUL AMERICANO',
+  'MUNDIAL CBJJE',
+  'IBJJF - EUROPE',
+];
+
+export const METHODS = [
+  'MÉTODO CONVENCIONAL - 76 min ativo + 20 intervalos = 96 min',
+  'MÉTODO SPRINT DE RESULTADOS - 31 min ativo + 5 min intervalos = 36 min',
+];
+
+export const ACCOMPLISHMENT = [
+  'Há mais de 10 anos, vivi o JJ na mais alta performance, me desenvolvendo através de treinos que duravam 4 a 6 horas/dia 5x semana.',
+  'Quem me ensinou possuía 30 anos de JJ old school, e o outro possuía uma didática digna de um chefe da PRF RJ-SP, ensinado em sua base por Royler Gracie.',
+  ' Formado em ed.física e expert em experiência do cliente.',
+];
+export const TARGETSUBJ = [
+  'Extremamente atarefado com rotinas muito monótonas de trabalho.',
+  'Se sente estressado devido as complicações no trabalho e em casa.',
+  'Não desenvolveu habilidades da juventude que gostaria de ter. Sente que perdeu essa fase da vida, pelos compromissos, faculdade e/ou carreira.',
+  'Não tem desafios novos para se motivar ao crescimento.',
+  'Vive para a família e trabalho, e sente falta de um momento de homem na semana.',
+];
+export const ACCHIEVEMENT = [
+  'Se sentir desafiado a melhorar e fortalescer a cada dia.',
+  'Sair do sedentarismo e se sentir bem, mais produtivo e energizado.',
+  'Criar autoconfiança e reconhecer como ser capaz de realizar atos poderosos.',
+  'Aliviar o estresse e se sentir mais no controle.',
+  'Criar mais disciplina e deteminação para conquistar seu sonho.',
+];
